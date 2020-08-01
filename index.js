60b04f931c2670eb9a31dc6a90f07ff54c826627
zacariaspaclibar <zacariaspaclibar@gmail.com>
2020-08-01 12:16:01 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function DiceChallenge(){
+     var randomNumber1 = Math.round((Math.random() * 5) + 1);
+     document.querySelector(".img1").setAttribute("src" , "images/dice"+randomNumber1+".png");
+     
+     var randomNumber2 = Math.round((Math.random() * 5) + 1);
+     document.querySelector(".img2").setAttribute("src" , "images/dice"+randomNumber2+".png");
+     if (randomNumber1 > randomNumber2){
+         document.querySelector("h1").textContent = "Player 1 Wins!";
+        }else if (randomNumber2 > randomNumber1){
+            document.querySelector("h1").textContent = "Player 2 Wins!";
+        }else{
+            document.querySelector("h1").textContent = "Draw!";
+        }
+    }
+    
+    
+    
